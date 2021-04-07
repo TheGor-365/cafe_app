74a0a481988fbda1cd135020ba8ff463c81216b5
Gor <7495500@gmail.com>
2021-04-07 23:32:52 +0300

    Add navigation, controllers models and templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :products
+
   devise_for :users
+
   root to: 'pages#home'
 
-  get 'pages/catering'
-  get 'pages/info'
-  
+  get 'catering', to: 'pages#catering'
+  get 'info', to: 'pages#info'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+  { username: 'sarah',    email: 'sarah@example.com',     password: 'Password', password_confirmation: 'password', admin: true },
+  { username: 'emily',    email: 'emily@example.com',     password: 'Password' },
+  { username: 'melanie',  email: 'melanie@example.com',   password: 'Password' },
+  { username: 'caroline', email: 'caroline@example.com',  password: 'Password' },
+  { username: 'bridget',  email: 'bridget@example.com',   password: 'Password' }
+])
+
+25.times do |i|
+  Product.ceate!(
+    name: 'Pizza',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    price: 11.99,
+    catering_item: true
+  )
+end
